607937252cc713604cc1f91f6fedcc5318b72d32
odlemur044-png <odlemur044@gmail.com>
2025-12-14 22:35:08 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'gunluk-takip-v1';
+const FILES_TO_CACHE = [
+  './',
+  'takip.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
